7f4488a8f0f53a2bda8f47af78995cca75a99180
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-11-22 23:08:06 +0100

    inicio medico

--- Assets/@MyAssets/Scripts/EnemyInteligent.cs
+++ Assets/@MyAssets/Scripts/EnemyInteligent.cs
@@ -7,10 +7,13 @@ public class EnemyInteligent : MonoBehaviour
 {
     public Transform target;
     public float Velocity;
+    public float velocidadMedico;
     public NavMeshAgent IA;
     public Animator anim;
     public GameObject portonObject;
     public Porton porton;
+
+    private bool haSidoVisto = false;
 
     void Start()
     {
@@ -19,7 +22,20 @@ public class EnemyInteligent : MonoBehaviour
 
     void Update()
     {
-        IA.speed = Velocity;
+        if (CompareTag("aldeano"))
+        {
+            IA.speed = Velocity;
+            GestionarAldeano();
+        }
+        else if (CompareTag("medico"))
+        {
+            IA.speed = velocidadMedico;
+            GestionarMedico();
+        }
+    }
+
+    void GestionarAldeano()
+    {
         if (porton != null && porton.EstaActivo())
         {
             IA.SetDestination(portonObject.transform.position);
@@ -28,8 +44,32 @@ public class EnemyInteligent : MonoBehaviour
         {
             IA.SetDestination(target.position);
         }
-        
-        if(IA.velocity == Vector3.zero)
+
+        if (IA.velocity == Vector3.zero)
+        {
+            anim.SetBool("estaAtacando", true);
+        }
+        else
+        {
+            anim.SetBool("estaAtacando", false);
+        }
+    }
+
+    void GestionarMedico()
+    {
+        if (!haSidoVisto)
+        {
+            anim.SetBool("aSidoVisto", false);
+            IA.SetDestination(target.position);
+            DetectarJugador();
+        }
+        else
+        {
+            anim.SetBool("aSidoVisto", true);
+            IA.SetDestination(target.position);
+        }
+
+        if (IA.velocity == Vector3.zero && haSidoVisto)
         {
             anim.SetBool("estaAtacando", true);
         }
@@ -41,6 +81,28 @@ public class EnemyInteligent : MonoBehaviour
 
     public void hit()
     {
-        porton.RecibirDanio(20);
+        if (porton != null)
+        {
+            porton.RecibirDanio(20);
+        }
+    }
+
+    void DetectarJugador()
+    {
+        Vector3 direccionAlJugador = Camera.main.transform.position - transform.position;
+        float angulo = Vector3.Angle(transform.forward, direccionAlJugador);
+
+        if (angulo < 60f)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direccionAlJugador.normalized, out hit, 20f))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    haSidoVisto = true;
+                    anim.SetBool("aSidoVisto", true);
+                }
+            }
+        }
     }
 }
