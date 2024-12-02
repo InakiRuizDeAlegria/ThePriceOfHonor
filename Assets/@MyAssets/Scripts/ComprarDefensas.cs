c014837bdda6787450c0224eac49da95d80ec4fd
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-12-02 14:15:05 +0100

    inicio comprar defensas

---
+++ Assets/@MyAssets/Scripts/ComprarDefensas.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComprarDefensas : MonoBehaviour
+{
+    public TextMeshProUGUI portonText;
+    public TextMeshProUGUI barricada;
+    public TextMeshProUGUI textoDinero;
+    public Button botonComprar;
+    
+    public Porton porton;
+    public GameObject barricadaPrefab;
+
+    private string opcionSeleccionada = "";
+
+    public int precioReparacion = 200;
+    public int preciobarricada = 100;
+
+    public MenuManagerTexto gestorMenu;
+
+    void Start()
+    {
+        portonText.text = $"1. Espada Superior - ${precioReparacion}";
+        barricada.text = $"2. Espada Inicial - ${preciobarricada}";
+
+        portonText.GetComponent<Button>().onClick.AddListener(() => SeleccionarOpcion("portonText"));
+        barricada.GetComponent<Button>().onClick.AddListener(() => SeleccionarOpcion("barricada"));
+        botonComprar.onClick.AddListener(ComprarObjeto);
+
+        ActualizarDineroUI();
+    }
+
+    void SeleccionarOpcion(string opcion)
+    {
+        opcionSeleccionada = opcion;
+
+        ResetearColores();
+        if (opcion == "portonText")
+            portonText.color = Color.green;
+        else if (opcion == "barricada")
+            barricada.color = Color.green;
+    }
+
+    void ResetearColores()
+    {
+        portonText.color = Color.white;
+        barricada.color = Color.white;
+    }
+
+    void ComprarObjeto()
+    {
+        int precio = 0;
+
+        if (opcionSeleccionada == "portonText")
+            precio = precioReparacion;
+        else if (opcionSeleccionada == "barricada")
+            precio = preciobarricada;
+
+        if (gestorMenu.dineroTotal >= precio)
+        {
+            gestorMenu.dineroTotal -= precio;
+            Vector3 posicionMesa = mesaTransform.position + new Vector3(0, 1, 0);
+
+            if (opcionSeleccionada == "porton")
+                porton.Reparar(100);
+            else if (opcionSeleccionada == "barricada")
+                barricadaPrefab.SetActive(true);
+
+            ActualizarDineroUI();
+        }
+    }
+
+    void ActualizarDineroUI()
+    {
+        textoDinero.text = $"Dinero: ${gestorMenu.dineroTotal}";
+    }
+
+}
