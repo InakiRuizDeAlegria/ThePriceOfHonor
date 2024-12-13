3eb56096b8f0c54b59a0fa0c183c1f4f3344d506
HugoReyIn <hugo.rey@opendeusto.es>
2024-12-13 22:38:11 +0100

    creada la funcionalidad de la escena de muerte

---
+++ Assets/@MyAssets/Scripts/DeadSceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class DeadSceneManager : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
